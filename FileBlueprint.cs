49b35fc65ed7ca08000914812292817fce26f164
Diego Hartmann <diego.hartmann@vexpro.com.br>
2023-01-30 21:50:44 -0300

    update

--- FileBlueprint.cs
+++ FileBlueprint.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace FileBuilder
 {
@@ -114,6 +115,7 @@ namespace FileBuilder
 
 
         #region =========== PUBLIC METHODS ===============================================================
+
 
         /// <summary> Adds text to the Content property. </summary>
         /// <param name="content">The content string to be added.</param>
