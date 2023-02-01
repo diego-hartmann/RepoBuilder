a278d8a0b347d8ffa4a71276641a9b1ee362387f
diego-hartmann <dtghartmann@gmail.com>
2023-01-31 22:33:35 -0300

    extention type extention method updated

--- Extention.cs
+++ Extention.cs
@@ -1,10 +1,20 @@
 ﻿namespace FileBuilder
 {
-    /// <summary> Extention type of the file. </summary>
+    /// <summary> 
+    /// Extention type of the file.
+    /// Edit it Edit as you see fit.
+    /// Just don't forget to update the ToExtentionString method as well.
+    /// </summary>
     public enum Extention { CSharp, HTML, CSS, JavaScript, Text, Bat, Python }
 
     internal static class EnumToString
     {
+        /// <summary>
+        /// Converts the extention type enum value into its respective string.
+        /// Eg. (Extention.JavaScript) => ".js"
+        /// </summary>
+        /// <param name="extentionValue">The enum value to be converted.</param>
+        /// <returns></returns>
         internal static string ToExtentionString(this Extention extentionValue)
         {
             switch (extentionValue)
