bd3378c4f82b36b3fb20035ea3bf69b59635e241
diego-hartmann <dtghartmann@gmail.com>
2023-02-15 13:05:03 -0300

    commented: fileBlueprint.cs

--- Files/FileBlueprint.cs
+++ Files/FileBlueprint.cs
@@ -1,21 +1,27 @@
-﻿using System.IO;
-
-namespace FileBuilder
+﻿namespace FileBuilder
 {
     public class FileBlueprint : Files
     {
 
-        #region =========== CONSTRUCTORS ===============================================================
+
+
+        #region ===========- CONSTRUCTORS -===============================================================
         /// <summary> Creates a virtual file that can be edited before being really created into your local machine. </summary>
         /// <param name="location">The path the real file will be created in.</param>
         /// <param name="name">The name the real file will have.</param>
         /// <param name="extention">The extention of the file.</param>
         public FileBlueprint(string name, Extention extention) => ConstructorForFile(name, extention);
-        #endregion =======================================================================================
+        #endregion _______________________________________________________________________________________
 
 
 
-        #region =========== PUBLIC METHODS ===============================================================
+
+
+
+
+
+
+        #region ===========- PUBLIC METHODS -===============================================================
         /// <summary> Creates another blueprint based on this. </summary>
         /// <returns>Returns a new object with the same properties.</returns>
         public FileBlueprint GetCopy()
@@ -37,10 +43,9 @@ namespace FileBuilder
 
         /// <summary>
         /// Removes the file from its parent child list.
-        /// The next Build() will delete it from the directory.
+        /// The next Build() will delete it from that directory.
         /// </summary>
-        public void ExitFolder() => FolderParent?.Remove(this);
-        #endregion =======================================================================================
-
+        public void LeaveFolder() => FolderParent?.Remove(this);
+        #endregion _______________________________________________________________________________________
     }
 }
