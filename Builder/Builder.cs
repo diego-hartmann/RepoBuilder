a8e4479d3274496c5de0ea81917fe196ac4994a9
diego-hartmann <dtghartmann@gmail.com>
2023-02-15 13:00:29 -0300

    Builder.cs commented

--- Builder/Builder.cs
+++ Builder/Builder.cs
@@ -2,7 +2,10 @@
 {
     public static class Builder
     {
+        /// <summary> Creates of updates this Container and its content (folders and files). </summary>
         public static void Build(this ContainerBlueprint container) => container.Build();
+
+        /// <summary> Deletes this Container and its content (folders and files). </summary>
         public static void Unbuild(this ContainerBlueprint container) => container.Unbuild();
     }
 }
